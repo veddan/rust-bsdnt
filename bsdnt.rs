a352e4fd54ad0211f81828ef32370b4d9f1088c0
Viktor Dahl <pazaconyoman@gmail.com>
2014-02-07 21:27:48 +0100

    Use 4 space indentation

--- bsdnt.rs
+++ bsdnt.rs
@@ -51,7 +51,7 @@ extern "C" {
     fn zz_is_zero(r: zz_srcptr) -> c_int;
 
     // fn zz_random
-    
+
     fn zz_seti(r: zz_ptr, c: sword_t);
     fn zz_set(r: zz_ptr, a: zz_srcptr);
     fn zz_neg(r: zz_ptr, a: zz_srcptr);
@@ -85,11 +85,11 @@ unsafe fn zz_equal(a: zz_srcptr, b: zz_srcptr) -> bool {
 
 macro_rules! binop_new(
     ($a:expr $b:expr $fun:expr) => (
-      unsafe {
+        unsafe {
         let mut res = Bsdnt::new();
         $fun(&mut res.zz, &$a.zz, &$b.zz);
         res
-      }
+        }
     );
 )
 
@@ -103,19 +103,19 @@ impl Drop for Bsdnt {
 
 impl Bsdnt {
     fn new() -> Bsdnt {
-      unsafe {
+        unsafe {
         let mut zz = std::unstable::intrinsics::uninit();
         zz_init(&mut zz);
         Bsdnt { zz: zz }
-      }
+        }
     }
 
     fn new_reserve(words: uint) -> Bsdnt {
-      unsafe {
+        unsafe {
         let mut zz = std::unstable::intrinsics::uninit();
         zz_init_fit(&mut zz, words as len_t);
         Bsdnt { zz: zz }
-      }
+        }
     }
 
     fn reserve(&mut self, words: uint) { unsafe { zz_fit(&mut self.zz, words as len_t); } }
@@ -124,29 +124,29 @@ impl Bsdnt {
 
 impl Eq for Bsdnt {
     fn eq(&self, other: &Bsdnt) -> bool {
-      unsafe { return zz_equal(&self.zz, &other.zz); }
+        unsafe { return zz_equal(&self.zz, &other.zz); }
     }
 }
 
 impl Ord for Bsdnt {
     fn lt(&self, other: &Bsdnt) -> bool {
-      let cmp = unsafe { zz_cmp(&self.zz, &other.zz) };
-      cmp < 0
+        let cmp = unsafe { zz_cmp(&self.zz, &other.zz) };
+        cmp < 0
     }
 }
 
 // This is pretty bad
 impl Orderable for Bsdnt {
     fn min(&self, other: &Bsdnt) -> Bsdnt {
-      if self < other { self } else { other }.clone()
+        if self < other { self } else { other }.clone()
     }
 
     fn max(&self, other: &Bsdnt) -> Bsdnt {
-      if self > other { self } else { other }.clone()
+        if self > other { self } else { other }.clone()
     }
 
     fn clamp(&self, min: &Bsdnt, max: &Bsdnt) -> Bsdnt {
-      if self < min { min } else if self > max { max } else { self }.clone()
+        if self < min { min } else if self > max { max } else { self }.clone()
     }
 }
 
@@ -164,26 +164,26 @@ impl Mul<Bsdnt, Bsdnt> for Bsdnt {
 
 impl Div<Bsdnt, Bsdnt> for Bsdnt {
     fn div(&self, other: &Bsdnt) -> Bsdnt {
-      if other.is_zero() { fail!("division by 0"); }
-      binop_new!(self other zz_div)
+        if other.is_zero() { fail!("division by 0"); }
+        binop_new!(self other zz_div)
     }
 }
 
 impl Neg<Bsdnt> for Bsdnt {
     fn neg(&self) -> Bsdnt {
-      unsafe {
+        unsafe {
         let mut res = Bsdnt::new_reserve(self.zz.size as uint);
         zz_neg(&mut res.zz, &self.zz);
         res
-      }
+        }
     }
 }
 
 impl Rem<Bsdnt, Bsdnt> for Bsdnt {
     // TODO This is less efficient than it could be, possibly implement with nn_???
     fn rem(&self, other: &Bsdnt) -> Bsdnt {
-      let (_, rem) = self.div_rem(other);
-      rem
+        let (_, rem) = self.div_rem(other);
+        rem
     }
 }
 
@@ -191,49 +191,49 @@ impl Num for Bsdnt { }
 
 impl Signed for Bsdnt {
     fn abs(&self) -> Bsdnt {
-      unsafe {
+        unsafe {
         if zz_cmpi(&self.zz, 0) >= 0 {
-          self.clone()
+            self.clone()
         } else {
-          -self
+            -self
         }
-      }
+        }
     }
 
     fn signum(&self) -> Bsdnt {
-      let c = unsafe { zz_cmpi(&self.zz, 0) };
-      if c < 0 {
+        let c = unsafe { zz_cmpi(&self.zz, 0) };
+        if c < 0 {
         FromPrimitive::from_int(-1)
-      } else if c == 0 {
+        } else if c == 0 {
         FromPrimitive::from_int(0)
-      } else {
+        } else {
         FromPrimitive::from_int(1)
-      }.unwrap()
+        }.unwrap()
     }
 
     fn is_positive(&self) -> bool {
-       unsafe { zz_cmpi(&self.zz, 0) > 0 }
+         unsafe { zz_cmpi(&self.zz, 0) > 0 }
     }
-    
+
     fn is_negative(&self) -> bool {
-       unsafe { zz_cmpi(&self.zz, 0) < 0 }
+         unsafe { zz_cmpi(&self.zz, 0) < 0 }
     }
 
     fn abs_sub(&self, other: &Bsdnt) -> Bsdnt {
-      if self < other { return Zero::zero(); }
-      *self - *other
+        if self < other { return Zero::zero(); }
+        *self - *other
     }
 }
 
 impl ToStr for Bsdnt {
     // This makes an unecessary copy
     fn to_str(&self) -> ~str {
-      unsafe {
+        unsafe {
         let cstr = zz_get_str(&self.zz);
         let rstr = std::str::raw::from_c_str(cstr as *c_char);
         std::libc::free(cstr as *mut c_void);
         rstr
-      }
+        }
     }
 }
 
@@ -241,38 +241,38 @@ impl Integer for Bsdnt {
     fn gcd(&self, other: &Bsdnt) -> Bsdnt { binop_new!(self other zz_gcd) }
 
     fn div_rem(&self, other: &Bsdnt) -> (Bsdnt, Bsdnt) {
-      if other.is_zero() { fail!("division by 0"); }
-      unsafe {
+        if other.is_zero() { fail!("division by 0"); }
+        unsafe {
         let mut quot = Bsdnt::new();
         let mut rem = Bsdnt::new();
         zz_divrem(&mut quot.zz, &mut rem.zz, &self.zz, &other.zz);
         (quot, rem)
-      }
+        }
     }
 
     fn div_floor(&self, other: &Bsdnt) -> Bsdnt { binop_new!(self other zz_div) }
 
     fn mod_floor(&self, other :&Bsdnt) -> Bsdnt { fail!("TODO mod_floor"); }
-    
+
     fn div_mod_floor(&self, other :&Bsdnt) -> (Bsdnt, Bsdnt) { fail!("TODO div_mod_floor"); }
-    
+
     fn lcm(&self, other :&Bsdnt) -> Bsdnt {
-      unsafe {
+        unsafe {
         let mut tmpa = self.clone();
         zz_mul(&mut tmpa.zz, &tmpa.zz, &other.zz);
         if zz_cmpi(&tmpa.zz, 0) < 0 {
-          zz_neg(&mut tmpa.zz, &tmpa.zz);
+            zz_neg(&mut tmpa.zz, &tmpa.zz);
         }
         let gcd = self.gcd(other);
         zz_div(&mut tmpa.zz, &tmpa.zz, &gcd.zz);
         tmpa
-      }
+        }
     }
-    
+
     fn mod_floor(&self, other :&Bsdnt) -> Bsdnt { fail!("TODO mod_floor"); }
 
     fn is_multiple_of(&self, other: &Bsdnt) -> bool {
-      !other.is_zero() && (*self % *other).is_zero()
+        !other.is_zero() && (*self % *other).is_zero()
     }
 
     fn is_even(&self) -> bool { unsafe { nn_bit_test(*(&self.zz.n) as nn_src_t, 0) == 0 } }
@@ -282,11 +282,11 @@ impl Integer for Bsdnt {
 
 impl One for Bsdnt {
     fn one() -> Bsdnt {
-      unsafe {
+        unsafe {
         let mut res = Bsdnt::new();
         zz_seti(&mut res.zz, 1);
         res
-      }
+        }
     }
 }
 
@@ -294,29 +294,29 @@ impl Zero for Bsdnt {
     fn zero() -> Bsdnt { Bsdnt::new() }
 
     fn is_zero(&self) -> bool {
-      let n = unsafe { zz_is_zero(&self.zz) };
-      return n == 1;
+        let n = unsafe { zz_is_zero(&self.zz) };
+        return n == 1;
     }
 }
 
 impl FromStr for Bsdnt {
     fn from_str(s: &str) -> Option<Bsdnt> {
-      if s.len() == 0 { return None; }
-      // `zz_set_str` is a bit weird.
-      // It uses only the prefix of `s` matching `\-?[0-9]*` and doesn't report errors.
-      // So we validate ourselved.
-      let mut it = s.chars();
-      let first = it.next().unwrap();  // We know it's not empty
-      if first != '-' && !first.is_digit() { return None; }
-      for c in it {
+        if s.len() == 0 { return None; }
+        // `zz_set_str` is a bit weird.
+        // It uses only the prefix of `s` matching `\-?[0-9]*` and doesn't report errors.
+        // So we validate ourselved.
+        let mut it = s.chars();
+        let first = it.next().unwrap();  // We know it's not empty
+        if first != '-' && !first.is_digit() { return None; }
+        for c in it {
         if !c.is_digit() { return None; }
-      }
+        }
 
-      let mut ret = Bsdnt::new();
-      unsafe {
+        let mut ret = Bsdnt::new();
+        unsafe {
         s.with_c_str(|cs| { zz_set_str(&mut ret.zz, cs) });
-      }
-      return Some(ret);
+        }
+        return Some(ret);
     }
 }
 
@@ -324,46 +324,46 @@ impl FromStr for Bsdnt {
 fn from_i64(n: i64) -> Option<Bsdnt> {
     let mut res = Bsdnt::new();
     unsafe { zz_seti(&mut res.zz, n as sword_t); }
-    Some(res)    
+    Some(res)
 }
 
 #[cfg(target_word_size = "64")]
 impl FromPrimitive for Bsdnt {
     fn from_i64(n: i64) -> Option<Bsdnt> { from_i64(n) }
-    
+
     // TODO Do this properly, allocating is just silly
     fn from_u64(n: u64) -> Option<Bsdnt> {
-      unsafe {
+        unsafe {
         if n < std::i64::MAX as u64 {
-          let mut res = Bsdnt::new();
-          zz_seti(&mut res.zz, n as sword_t);
-          return Some(res);
+            let mut res = Bsdnt::new();
+            zz_seti(&mut res.zz, n as sword_t);
+            return Some(res);
         } else {
-          // Split the overly large number into three parts and add them
-          let a = n / 3;
-          let b = a;
-          let rest = n - (a + b);
-          let tmpa = from_i64(a as sword_t).unwrap();
-          let tmpb = tmpa + from_i64(b as sword_t).unwrap();
-          let mut res = Bsdnt::new();
-          zz_addi(&mut res.zz, &tmpb.zz, rest as sword_t);
-          return Some(res);
+            // Split the overly large number into three parts and add them
+            let a = n / 3;
+            let b = a;
+            let rest = n - (a + b);
+            let tmpa = from_i64(a as sword_t).unwrap();
+            let tmpb = tmpa + from_i64(b as sword_t).unwrap();
+            let mut res = Bsdnt::new();
+            zz_addi(&mut res.zz, &tmpb.zz, rest as sword_t);
+            return Some(res);
         }
-      }
+        }
     }
 }
 
 impl Clone for Bsdnt {
     fn clone(&self) -> Bsdnt {
-      unsafe {
+        unsafe {
         let mut ret = Bsdnt::new();
         zz_set(&mut ret.zz, &self.zz);
         ret
-      }
+        }
     }
 
     fn clone_from(&mut self, source: &Bsdnt) {
-      unsafe { zz_set(&mut self.zz, &source.zz); };
+        unsafe { zz_set(&mut self.zz, &source.zz); };
     }
 }
 
@@ -374,151 +374,151 @@ mod test {
 
     #[test]
     fn test_clone_from() {
-      let mut x: Bsdnt = FromPrimitive::from_i64(4000).unwrap();
-      let y: Bsdnt = FromPrimitive::from_i64(19000).unwrap();
-      assert!(x != y);
-      x.clone_from(&y);
-      assert!(x == y);
+        let mut x: Bsdnt = FromPrimitive::from_i64(4000).unwrap();
+        let y: Bsdnt = FromPrimitive::from_i64(19000).unwrap();
+        assert!(x != y);
+        x.clone_from(&y);
+        assert!(x == y);
     }
-    
+
     #[test]
     fn test_clone() {
-      let mut x: Bsdnt = from_str("-31232313").unwrap();
-      let y = x.clone();
-      let z: Bsdnt = from_str("3000").unwrap();
-      assert!(x == y);
-      x = x + z;
-      assert!(x != y);
+        let mut x: Bsdnt = from_str("-31232313").unwrap();
+        let y = x.clone();
+        let z: Bsdnt = from_str("3000").unwrap();
+        assert!(x == y);
+        x = x + z;
+        assert!(x != y);
     }
 
     #[test]
     fn test_from_str_valid() {
-      let x: Bsdnt = FromPrimitive::from_i64(4000).unwrap();
-      let y: Bsdnt = FromPrimitive::from_i64(-19000).unwrap();
-      let xi: Bsdnt = from_str("4000").unwrap();
-      let yi: Bsdnt = from_str("-19000").unwrap();
-      assert!(x != yi);
-      assert!(y != xi);
-      assert!(x == xi);
-      assert!(y == yi);
+        let x: Bsdnt = FromPrimitive::from_i64(4000).unwrap();
+        let y: Bsdnt = FromPrimitive::from_i64(-19000).unwrap();
+        let xi: Bsdnt = from_str("4000").unwrap();
+        let yi: Bsdnt = from_str("-19000").unwrap();
+        assert!(x != yi);
+        assert!(y != xi);
+        assert!(x == xi);
+        assert!(y == yi);
     }
 
     #[test]
     fn test_from_str_invalid() {
-      let xi = from_str::<Bsdnt>("4000hej");
-      assert!(xi.is_none());
+        let xi = from_str::<Bsdnt>("4000hej");
+        assert!(xi.is_none());
     }
 
     #[test]
     fn test_ord() {
-      let x: Bsdnt = from_str("-31232313").unwrap();
-      let y: Bsdnt = from_str("123432843254375345743857834").unwrap();
-      let z: Bsdnt = from_str("321382138321312333333901381").unwrap();
-      assert!(x < y && x < z && y < z);
-      assert!(x <= x && x <= y && x <= z && y <= z);
-      assert!(z > y && z > x && y > x);
-      assert!(z >= z && z >= y && z >= x && y >= x);
+        let x: Bsdnt = from_str("-31232313").unwrap();
+        let y: Bsdnt = from_str("123432843254375345743857834").unwrap();
+        let z: Bsdnt = from_str("321382138321312333333901381").unwrap();
+        assert!(x < y && x < z && y < z);
+        assert!(x <= x && x <= y && x <= z && y <= z);
+        assert!(z > y && z > x && y > x);
+        assert!(z >= z && z >= y && z >= x && y >= x);
     }
 
     #[test]
     fn test_zero_init() {
-      let x = Bsdnt::new();
-      let y: Bsdnt = from_str("0").unwrap();
-      let z: Bsdnt = Zero::zero();
-      assert!(x == y);
-      assert!(z == y);
+        let x = Bsdnt::new();
+        let y: Bsdnt = from_str("0").unwrap();
+        let z: Bsdnt = Zero::zero();
+        assert!(x == y);
+        assert!(z == y);
     }
 
     #[test]
     fn test_one() {
-      let x: Bsdnt = FromPrimitive::from_i64(1).unwrap();
-      assert!(x == One::one());
+        let x: Bsdnt = FromPrimitive::from_i64(1).unwrap();
+        assert!(x == One::one());
     }
 
     #[test]
     #[should_fail]
     fn test_div_zero() {
-      let x: Bsdnt = Zero::zero();
-      x / x;
+        let x: Bsdnt = Zero::zero();
+        x / x;
     }
-    
+
     #[test]
     #[should_fail]
     fn test_rem_zero() {
-      let x: Bsdnt = Zero::zero();
-      x % x;
+        let x: Bsdnt = Zero::zero();
+        x % x;
     }
 
     #[test]
     fn test_neg() {
-      let x: Bsdnt = from_str("32982908").unwrap();
-      let y: Bsdnt = from_str("-32982908").unwrap();
-      assert!(x.neg() == y);
+        let x: Bsdnt = from_str("32982908").unwrap();
+        let y: Bsdnt = from_str("-32982908").unwrap();
+        assert!(x.neg() == y);
     }
 
-    #[test]  
+    #[test]
     fn test_even() {
-      let x: Bsdnt = from_str("8").unwrap();
-      let y: Bsdnt = from_str("9").unwrap();
-      let z: Bsdnt = from_str("-8").unwrap();
-      let p: Bsdnt = from_str("-9").unwrap();
-      let q: Bsdnt = from_str("0").unwrap();
-      assert!(x.is_even());
-      assert!(y.is_odd());
-      assert!(z.is_even());
-      assert!(p.is_odd());
-      assert!(q.is_even());
+        let x: Bsdnt = from_str("8").unwrap();
+        let y: Bsdnt = from_str("9").unwrap();
+        let z: Bsdnt = from_str("-8").unwrap();
+        let p: Bsdnt = from_str("-9").unwrap();
+        let q: Bsdnt = from_str("0").unwrap();
+        assert!(x.is_even());
+        assert!(y.is_odd());
+        assert!(z.is_even());
+        assert!(p.is_odd());
+        assert!(q.is_even());
     }
 
     #[test]
     fn test_lcm() {
-      let x: Bsdnt = FromPrimitive::from_i64(4).unwrap();
-      let y: Bsdnt = FromPrimitive::from_i64(6).unwrap();
-      let z: Bsdnt = FromPrimitive::from_i64(12).unwrap();
-      assert!(x.lcm(&y) == z);
+        let x: Bsdnt = FromPrimitive::from_i64(4).unwrap();
+        let y: Bsdnt = FromPrimitive::from_i64(6).unwrap();
+        let z: Bsdnt = FromPrimitive::from_i64(12).unwrap();
+        assert!(x.lcm(&y) == z);
 
-      let p: Bsdnt = FromPrimitive::from_i64(-5).unwrap();
-      let q: Bsdnt = FromPrimitive::from_i64(2).unwrap();
-      let r: Bsdnt = FromPrimitive::from_i64(10).unwrap();
-      assert!(p.lcm(&q) == r);
+        let p: Bsdnt = FromPrimitive::from_i64(-5).unwrap();
+        let q: Bsdnt = FromPrimitive::from_i64(2).unwrap();
+        let r: Bsdnt = FromPrimitive::from_i64(10).unwrap();
+        assert!(p.lcm(&q) == r);
     }
 
     #[test]
     fn test_gcd() {
-      let x: Bsdnt = FromPrimitive::from_i64(163231).unwrap();
-      let y: Bsdnt = FromPrimitive::from_i64(135749).unwrap();
-      assert!(x.gcd(&y) == FromPrimitive::from_i64(151).unwrap());
+        let x: Bsdnt = FromPrimitive::from_i64(163231).unwrap();
+        let y: Bsdnt = FromPrimitive::from_i64(135749).unwrap();
+        assert!(x.gcd(&y) == FromPrimitive::from_i64(151).unwrap());
     }
 
     #[test]
     fn test_abs() {
-      let x: Bsdnt = from_str("32982908").unwrap();
-      let y: Bsdnt = from_str("-32982908").unwrap();
-      assert!(x == x.abs());
-      assert!(x == y.abs());
+        let x: Bsdnt = from_str("32982908").unwrap();
+        let y: Bsdnt = from_str("-32982908").unwrap();
+        assert!(x == x.abs());
+        assert!(x == y.abs());
     }
 
     #[test]
     fn test_is_multiple_of() {
-      let x: Bsdnt = from_str("-9").unwrap();
-      let y: Bsdnt = from_str("3").unwrap();
-      let z: Bsdnt = from_str("7").unwrap();
-      assert!(x.is_multiple_of(&y));
-      assert!(!z.is_multiple_of(&y));
-      assert!(!z.is_multiple_of(&Zero::zero()));
+        let x: Bsdnt = from_str("-9").unwrap();
+        let y: Bsdnt = from_str("3").unwrap();
+        let z: Bsdnt = from_str("7").unwrap();
+        assert!(x.is_multiple_of(&y));
+        assert!(!z.is_multiple_of(&y));
+        assert!(!z.is_multiple_of(&Zero::zero()));
     }
 
     #[test]
     fn test_to_str() {
-      let a = "32982908";
-      let b = "-32982908";
-      let c = "0";
-      let x: Bsdnt = from_str(a).unwrap();
-      let y: Bsdnt = from_str(b).unwrap();
-      let z: Bsdnt = from_str(c).unwrap();
-      assert!(a == x.to_str());
-      assert!(b == y.to_str());
-      assert!(c == z.to_str());
+        let a = "32982908";
+        let b = "-32982908";
+        let c = "0";
+        let x: Bsdnt = from_str(a).unwrap();
+        let y: Bsdnt = from_str(b).unwrap();
+        let z: Bsdnt = from_str(c).unwrap();
+        assert!(a == x.to_str());
+        assert!(b == y.to_str());
+        assert!(c == z.to_str());
     }
 }
 
@@ -529,29 +529,29 @@ mod bench {
     use super::*;
     use std::iter::range_inclusive;
     use std::num::{One};
-    
+
     fn factorial<T: Integer+FromPrimitive>(n: uint) -> T {
-      let mut f: T = One::one();
-      for i in range_inclusive(1, n) {
-          f = f * FromPrimitive::from_uint(i).unwrap();
-      }
-      f
+        let mut f: T = One::one();
+        for i in range_inclusive(1, n) {
+            f = f * FromPrimitive::from_uint(i).unwrap();
+        }
+        f
     }
 
     #[bench]
     fn bench_factorial100(b: &mut extra::test::BenchHarness) {
-      b.iter(|| { factorial::<Bsdnt>(100); });
-    } 
-    
+        b.iter(|| { factorial::<Bsdnt>(100); });
+    }
+
     #[bench]
     fn bench_gcd_big_small(b: &mut extra::test::BenchHarness) {
-      let x: Bsdnt = from_str("34732948324832498731289739821694523473248923649327439812742891323213\
-                               38219038920183981391920839021890382109321903821312807439565786213821\
-                               32183219087390218309218309218390281097401274328473289472389479237431\
-                               31238129038920138902183902180382190389201843754983574389758934758943\
-                               4328948329048930284903275329857345894375897435897439857894375987345")
-                              .unwrap();
-      let y: Bsdnt = from_str("19").unwrap();
-      b.iter(|| { x.gcd(&y); });
-    } 
+        let x: Bsdnt = from_str("34732948324832498731289739821694523473248923649327439812742891323213\
+                                 38219038920183981391920839021890382109321903821312807439565786213821\
+                                 32183219087390218309218309218390281097401274328473289472389479237431\
+                                 31238129038920138902183902180382190389201843754983574389758934758943\
+                                 4328948329048930284903275329857345894375897435897439857894375987345")
+                                .unwrap();
+        let y: Bsdnt = from_str("19").unwrap();
+        b.iter(|| { x.gcd(&y); });
+    }
 }
